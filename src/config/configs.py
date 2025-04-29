b37f6f251b8e5b1c4acde4b8943fcb6d818bdde0
luzinsan <luzinsan@mail.ru>
2025-04-29 23:01:49 +0300

    feat: redesigned data preprocessing

--- src/config/configs.py
+++ src/config/configs.py
@@ -13,6 +13,14 @@ class Settings(BaseSettings):
     DEVICE: torch.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     NUM_WORKERS: int = 4
 
+    ITEM_DETAILS_COLUMNS: List[str] = [
+        'category_name', 
+        'im_desc', 
+        'pack', 
+        'bottle_volume_ml','state_bottle_cost','state_bottle_retail',
+        'sale_dollars','sale_bottles','sale_liters',
+    ]
+
 
     class Config:
         env_file = ".env"
